618c9c2042c11c9bf0eb2d074f3b3a6823722f5f
kaitlinruss <116702098+kaitlinruss@users.noreply.github.com>
2023-05-15 22:06:28 -0400

    Initial Save
    Saving Files

---
+++ JS/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $("nav .tab1").on("click", function(){
+        window.scrollTo(0, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("nav .tab2").on("click", function(){
+        window.scrollTo(0, 1900);
+    });
+});
+
+
+            var modal = document.getElementById("myModal");
+            
+            var btn = document.getElementById("myBtn");
+            
+            var span = document.getElementsByClassName("close")[0];
+
+            var submit = document.getElementById("submit");
+            
+            btn.onclick = function() {
+              modal.style.display = "block";
+            }
+            
+            span.onclick = function() {
+              modal.style.display = "none";
+            }
+
+            submit.onclick = function() {
+              modal.style.display = "none";
+            }
+            
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+              }
+            }
+
+            
+            function hamburger(x) {
+              x.classList.toggle("change");
+            }
+        
+
+
+
+$(".menu").click(function(){
+    $(this).find(".menuContent").toggleClass("dropdown")
+    $(this).siblings(".menu").find(".menuContent").removeClass("dropdown");
+  });
+
+
+  /*$(".toggle").click(function(){  
+    $(this).find(".toggleContainer").toggleClass("active");
+    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+});  */
+
